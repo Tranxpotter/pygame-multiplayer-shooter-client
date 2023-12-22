1327ab186c7de7d118334ae980bd3e7917473650
Tranxpotter <80389555+Tranxpotter@users.noreply.github.com>
2023-12-22 09:58:02 +0800

    modified:   client2.py 	new file:   communication.txt 	new file:   fonts/ARIAL.TTF 	new file:   fonts/ARIALBD.TTF 	new file:   fonts/ARIALBI.TTF 	new file:   fonts/ARIALI.TTF 	new file:   fonts/ARIALN.TTF 	new file:   fonts/ARIALNB.TTF 	new file:   fonts/ARIALNBI.TTF 	new file:   fonts/ARIALNI.TTF 	new file:   fonts/ARIBLK.TTF 	modified:   network.py 	modified:   theme.json

--- client2.py
+++ client2.py
@@ -12,17 +12,64 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 network = Network("ws://localhost:8765")
+network.start_connection()
 
 manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT), "theme.json")
 manager.set_visual_debug_mode(True)
 
-hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
-                                             text='Say Hello',
-                                             manager=manager)
-game_title = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(0, -30, 200, 100),
-                                         text="Test Game", manager=manager,
+ANCHOR_CENTER = {"centerx":"centerx", "centery":"centery"}
+game_title = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(0, 0, 1280, 100),
+                                         text="Multiplayer Shooter Test Game", manager=manager,
                                          anchors={'centerx': 'centerx',
-                                         'bottom': 'bottom'}, object_id=ObjectID("#game_title", "@title"))
+                                         'top': 'top'}, object_id=ObjectID("#game_title", "@title"))
+
+username_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(0, -50, 300, 50), 
+                                                     manager=manager, 
+                                                     object_id=ObjectID("#username_input", "@login_input"), 
+                                                     placeholder_text="username", 
+                                                     anchors=ANCHOR_CENTER)
+
+password_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect(0, 50, 300, 50), 
+                                                     manager=manager, 
+                                                     object_id=ObjectID("#password_input", "@login_input"), 
+                                                     placeholder_text="password", 
+                                                     anchors=ANCHOR_CENTER)
+password_input.set_text_hidden()
+
+error_display = pygame_gui.elements.UILabel(relative_rect=pygame.Rect(0, 150, 200, 50),
+                                            manager=manager,
+                                            object_id=ObjectID("#error_display", "@error_display"), 
+                                            text="", 
+                                            anchors=ANCHOR_CENTER)
+error_display.visible = 0
+
+signin_options = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(0, -100, 100, 50), 
+                                            manager=manager,
+                                            object_id=ObjectID("#signin_options", "@signin_options"),
+                                            text="login", 
+                                            anchors = ANCHOR_CENTER)
+
+submit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(180, 50, 50, 50), 
+                                             manager=manager,
+                                             object_id=ObjectID("#submit_button", "@submit_button"),
+                                             text="->",
+                                             anchors=ANCHOR_CENTER)
+
+
+def on_signin():
+    if not username_input.text:
+        error_display.visible = 1
+        error_display.set_text("username is required")
+        return
+    elif not password_input.text:
+        error_display.visible = 1
+        error_display.set_text("password is required")
+        return
+    
+    
+    
+
+
 
 run = True
 dt = 0
@@ -33,7 +80,29 @@ while run:
             run = False
             break
 
+        elif event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED:
+            if event.ui_object_id == "#username_input":
+                username_input.unfocus()
+                password_input.focus()
+            elif event.ui_object_id == "#password_input":
+                password_input.unfocus()
+                on_signin()
+        
+        elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_object_id == "#signin_options":
+                if signin_options.text == "login":
+                    signin_options.set_text("signup")
+                else:
+                    signin_options.set_text("login")
+            elif event.ui_object_id == "#submit_button":
+                on_signin()
+            
+
         manager.process_events(event)
+    
+    if not network.connected:
+        error_display.visible = 1
+        error_display.set_text("Not connected to server")
                 
     manager.update(dt)
 
--- network.py
+++ network.py
@@ -1,21 +1,58 @@
 import websockets
-from typing import Literal
-from dataclasses import dataclass
+from typing import Literal, Callable, Optional
 import asyncio
 import json
-
+from threading import Thread
 
 class Network:
     def __init__(self, uri:str) -> None:
         self.uri = uri
-        self.websocket:websockets.WebSocketClientProtocol = websockets.connect(self.uri)
+        self.websocket:websockets.WebSocketClientProtocol|None = None
+        self.connected = False
+        self.on_connection_error = None
+        self.on_error_do_default = True
+        self.on_reconnect = None
+        loop = asyncio.new_event_loop()
+        self._loop = loop
+        self._network_conn_thread = Thread(target=loop.run_forever).start()
     
+    def start_connection(self):
+        asyncio.run_coroutine_threadsafe(self.connect(), self._loop)
+
+    def set_on_connection_error(self, func:Callable, do_default = True):
+        self.on_connection_error = func
+        self.on_error_do_default = do_default
+
+    async def connect(self):
+        while not self.connected:
+            try:
+                self.websocket = await websockets.connect(self.uri)
+                self.connected = True
+            except ConnectionRefusedError:
+                self.connected = False
+                continue
+    
+    def set_on_reconnect(self, func:Callable):
+        self.on_reconnect = func
+
+    def _on_connection_error(self):
+        self.on_connection_error()
+        if not self.on_error_do_default:
+            return
+        asyncio.run_coroutine_threadsafe(self.connect(), self._loop)
     
     async def send(self, data):
-        await self.websocket.send(data)
+        try:
+            await self.websocket.send(data)
+        except websockets.ConnectionClosedError:
+            self._on_connection_error()
     
     async def recv(self):
-        return await self.websocket.recv()
+        try:
+            return await self.websocket.recv()
+        except websockets.ConnectionClosedError:
+            self._on_connection_error()
+        
 
 
 
